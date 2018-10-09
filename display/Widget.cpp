9f241df4652657e3e3cacdc870839bad6d7f1017
Juraj Podoba <juraj.podoba@gmail.com>
2018-10-09 07:31:26 +0200

    resizing

--- display/Widget.cpp
+++ display/Widget.cpp
@@ -2,11 +2,12 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QDebug>
+#include <QResizeEvent>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
-    main = new QLabel("X");
+    main = new QLabel("O");
     aux = new QLabel("y");
     auto layout = new QVBoxLayout;
     layout->addWidget(main);
@@ -16,7 +17,21 @@ Widget::Widget(QWidget *parent)
 
 void Widget::resizeEvent(QResizeEvent *event)
 {
-    qDebug() << "resize event:" << width() << height();
+        auto f = main->font();
+        QFontMetrics fm(f);
+
+        auto cur = fm.horizontalAdvance(main->text());
+        float factor = (float)(width()-1) / cur;
+
+        qDebug() << width() << height()
+             << main->width() << main->height()
+             << "font" << fm.horizontalAdvance(main->text()) << fm.height() << f.pointSizeF() << factor
+             << "eventprop" << event->spontaneous() << event->isAccepted();
+
+        f.setPointSizeF(f.pointSizeF()*factor);
+        if(factor < 0.9 || factor > 1.1)
+            main->setFont(f);
+
     QWidget::resizeEvent(event);
 }
 
