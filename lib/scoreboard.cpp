7c7babbf60c6dd46a5a261847963c46e79970066
Juraj Podoba <juraj.podoba@gmail.com>
2018-10-13 08:31:44 +0200

    project layout

---
+++ lib/scoreboard.cpp
@@ -0,0 +1,6 @@
+#include "scoreboard.h"
+
+
+Scoreboard::Scoreboard()
+{
+}
---
+++ lib/scoreboard.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QTcpServer>
+
+class Scoreboard
+{
+
+    QTcpServer listener;
+
+public:
+    Scoreboard();
+};
+
