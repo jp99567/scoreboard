e685c49571cfa5c7b24187265f3f938b87cbc2b5
Juraj Podoba <juraj.podoba@gmail.com>
2018-10-15 08:21:08 +0200

    added protocol skeleton

--- lib/protocol.h
+++ lib/protocol.h
@@ -1,10 +1,53 @@
 #pragma once
 
 #include <cinttypes>
+#include <QObject>
 
 namespace scoreboard
 {
 
 constexpr uint16_t port = 9233;
+
+class Protocol : public QObject
+{
+    Q_OBJECT
+public:
+    enum DisplayRole
+    {
+        No,
+        A1,
+        A2,
+        B1,
+        B2
+    };
+    Q_ENUM(DisplayRole)
+
+    enum Event
+    {
+        PlusA,
+        PlusB
+    };
+    Q_ENUM(Event)
+
+    enum ServerRequest
+    {
+        InfoDisplayRole,
+        AcceptEvent,
+        SetScore // ScoreA ScoreB SetA SetB Servis
+    };
+    Q_ENUM(ServerRequest)
+
+    enum ServerIndication
+    {
+        InfoScore, // ScoreA ScoreB SetA SetB Servis
+        SetMainDisplayText,
+        SetMainDisplayTextColor,
+        SetMainDisplayBgColor,
+        SetAuxDisplayText,
+        SetAuxDisplayTextColor,
+        SetAuxDisplayBgColor,
+        PlaySound
+    };
+};
 
 }
